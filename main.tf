56ec644b706d2191cd225edfa07385681cc2d6da
Ducas Francis <ducas@ducasfrancis.com>
2019-03-18 21:17:25 +1100

    create an aks cluster with advanced networking on a custom subnet with kubenet

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 terraform {}
 
-variable "cluster_name" {}
-variable "resource_group_name" {}
-variable "default_node_count" {}
-
 provider "external" {}
+provider "azurerm" {}
+
+variable "resource_group_name" {}
+variable "resource_group_location" {}
+variable "service_principal_client_id" {}
+variable "service_principal_client_secret" {}
+variable "cluster_name" {}
+variable "kubernetes_version" {}
+variable "default_node_count" {}
+variable "node_vm_size" {}
+variable "max_pods" {}
+variable "vnet_subnet_id" {}
+variable "log_analytics_workspace_id" {}
 
 data "external" "aks_cluster" {
   program = ["pwsh", "Get-AksNodeCount.ps1"]
@@ -16,6 +25,40 @@ data "external" "aks_cluster" {
   }
 }
 
-output "count" {
-  value = "${data.external.aks_cluster.result.node_count}"
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = "${var.cluster_name}"
+  location            = "${var.resource_group_location}"
+  resource_group_name = "${var.resource_group_name}"
+  dns_prefix          = "${var.cluster_name}"
+  kubernetes_version  = "${var.kubernetes_version}"
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 1
+    vm_size         = "${var.node_vm_size}"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+    vnet_subnet_id  = "${var.vnet_subnet_id}"
+    max_pods        = "${var.max_pods}"
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = "${var.log_analytics_workspace_id}"
+    }
+  }
+
+  service_principal {
+    client_id     = "${var.service_principal_client_id}"
+    client_secret = "${var.service_principal_client_secret}"
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
--- my-env.tfvars
+++ my-env.tfvars
@@ -1,5 +1,15 @@
 
-cluster_name = "my-aks-cluster-1"
 resource_group_name = "my-resource-group"
-default_node_count = "4"
+resource_group_location = "australiaeast"
 
+service_principal_client_id = "00000000-0000-0000-0000-000000000000"
+service_principal_client_secret = "00000000000000000000000000000000"
+
+cluster_name = "my-aks-cluster-1"
+kubernetes_version = "1.11.7"
+default_node_count = "4"
+node_vm_size = "Standard_D1_v2"
+max_pods = 100
+vnet_subnet_id = "/subscriptions/[my-subscription-id]/resourceGroups/[my-subnet-rg]/providers/Microsoft.Network/virtualNetworks/[my-vnet-name]/subnets/[my-subnet-name]"
+
+log_analytics_workspace_id = "/subscriptions/[my-subscription-id]/resourcegroups/[my-log-analytics-rg]/providers/microsoft.operationalinsights/workspaces/[my-workspace-name]"
